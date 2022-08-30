16b61328d53b6c7a16ea665d1740e0a20dffe278
Tanmay Deo <tanmay.deo@ril.com>
2022-08-30 16:50:10 +0530

    Delegates and lifecycle added

---
+++ AppDelegate , SceneDelagate , AppLifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppDelegate , SceneDelagate , AppLifeCycle
+//
+//  Created by Tanmay Deo on 30/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
